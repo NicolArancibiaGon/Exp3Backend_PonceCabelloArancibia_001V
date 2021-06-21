cd345f87cacde0c7d43dc6fb2cd2b7b7049953af
Nicol Arancibia <ni.arancibiag@duocuc.cl>
2021-06-20 22:17:59 -0400

    Se crea model Usuario, se conecta con la BDD Oracle y se crea super usuario

--- exp3/voluntarios/models.py
+++ exp3/voluntarios/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    rutUsuario = models.IntegerField(primary_key=True, verbose_name='Rut de usuario')
+    nombreUsuario = models.CharField(max_length=50, verbose_name='Nombre de usuario')
+    fechaNac = models.DateField()
+    region = models.CharField(max_length=50)
+    comuna = models.CharField(max_length=50)
+    email = models.EmailField()
+    email2 = models.EmailField()
+    password = models.CharField(max_length=12)
+    password2 = models.CharField(max_length=12)
+    fono = models.CharField(max_length=12)
+    VOLUNTARIO = 'VO'
+    AYUDA = 'AY'
+    vol_ayuda_choices = [
+        (VOLUNTARIO, 'Quiero ser voluntario'),
+        (AYUDA, 'Necesito ayuda')
+    ]
+    vol_ayuda = models.CharField(
+        max_length=2,
+        choices= vol_ayuda_choices,
+        default=VOLUNTARIO,
+    )
+
+    def str(self):
+        return self.rutUsuario
